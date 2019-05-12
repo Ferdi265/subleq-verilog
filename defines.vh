3b1302d3bc6afe850f562204eaa07a4293872b06
Ferdinand Bachmann <theferdi265@gmail.com>
2019-05-12 19:33:01 +0200

    add intermediate states for waiting on memory

--- defines.vh
+++ defines.vh
@@ -1,12 +1,20 @@
 `define WORD_SIZE 16
 
-`define STATE_BITS 3
-`define FETCH_A   3'b000
-`define DEREF_A   3'b001
-`define FETCH_B   3'b010
-`define DEREF_B   3'b011
-`define STORE_SUB 3'b100
-`define FETCH_C   3'b101
-`define HALT      3'b110
+`define STATE_BITS 4
+`define FETCH_A    4'b0000
+`define WAIT_A     4'b0001
+`define FINISH_A   4'b0010
+`define DEREF_A    4'b0011
+`define WAIT_DA    4'b0100
+`define FETCH_B    4'b0101
+`define WAIT_B     4'b0110
+`define FINISH_B   4'b0111
+`define DEREF_B    4'b1000
+`define WAIT_DB    4'b1001
+`define STORE_SUB  4'b1010
+`define WAIT_STORE 4'b1011
+`define FETCH_C    4'b1100
+`define WAIT_C     4'b1101
+`define HALT       4'b1111
 
 `define INTERACTIVE ($test$plusargs("i") || $test$plusargs("interactive"))
--- subleq_controller.v
+++ subleq_controller.v
@@ -4,6 +4,7 @@ module subleq_controller(
     input clk,
     input areset,
 
+    input ack,
     input halt,
     output [`STATE_BITS - 1 : 0] control_word
 );
@@ -15,12 +16,19 @@ module subleq_controller(
 
     assign next_state =
         halt ? `HALT :
-        state == `FETCH_A ? `DEREF_A :
-        state == `DEREF_A ? `FETCH_B :
-        state == `FETCH_B ? `DEREF_B :
-        state == `DEREF_B ? `STORE_SUB :
-        state == `STORE_SUB ? `FETCH_C :
-        state == `FETCH_C ? `FETCH_A :
+        state == `FETCH_A && ack ? `WAIT_A :
+        state == `WAIT_A && !ack ? `FINISH_A :
+        state == `FINISH_A ? `DEREF_A :
+        state == `DEREF_A && ack ? `WAIT_DA :
+        state == `WAIT_DA && !ack ? `FETCH_B :
+        state == `FETCH_B && ack ? `WAIT_B :
+        state == `WAIT_B && !ack ? `FINISH_B :
+        state == `FINISH_B ? `DEREF_B :
+        state == `DEREF_B && ack ? `WAIT_DB :
+        state == `WAIT_DB && !ack ? `STORE_SUB :
+        state == `STORE_SUB && ack ? `WAIT_STORE :
+        state == `WAIT_STORE && !ack ? `FETCH_C :
+        state == `FETCH_C && ack ? `WAIT_C :
         state;
 
     assign control_word = state;
